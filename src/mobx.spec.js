0f830aae5bd58a72e3402146ff45f6b731aa10a6
Stefan Wille <github@stefanwille.com>
2018-03-17 11:41:55 +0100

    strict mode

--- src/mobx.spec.js
+++ src/mobx.spec.js
@@ -1,16 +1,22 @@
-const { observable, decorate, autorun, computed } = require("mobx");
+const {
+  configure,
+  decorate,
+  autorun,
+  computed,
+  extendObservable,
+  observable,
+  action
+} = require("mobx");
 
-describe("MobX Todo", () => {
+describe("MobX", () => {
+  configure({ enforceActions: true });
+
   describe("observable property", () => {
     class Todo {
       constructor() {
-        this.title = "";
+        extendObservable(this, { title: 0 });
       }
     }
-
-    decorate(Todo, {
-      title: observable
-    });
 
     const todo = new Todo();
 
@@ -21,40 +27,54 @@ describe("MobX Todo", () => {
   });
 
   describe("autorun()", () => {
-    let Todo;
-    let observerCalls = 0;
+    let Todo = class {
+      constructor() {
+        extendObservable(
+          this,
+          {
+            title: "",
+
+            setTitle(v) {
+              this.title = v;
+            }
+          },
+          {
+            setTitle: action
+          }
+        );
+      }
+    };
+
     let todo;
+    let observerCalls = 0;
     let disposer;
 
     beforeEach(() => {
       observerCalls = 0;
 
-      todo = observable({
-        title: ""
-      });
+      todo = new Todo();
 
       disposer = autorun(() => {
         observerCalls++;
         todo.title;
       });
-      expect(observerCalls).toBe(1);
     });
 
     it("lets a function run as a reaction to store changes", () => {
       // autorun() runs the callback function immediately to learn which properties it looks at.
       expect(observerCalls).toBe(1);
-      todo.title = "yeah";
+      todo.setTitle("yeah");
       expect(observerCalls).toBe(2);
-      todo.title = "great";
+      todo.setTitle("great");
       expect(observerCalls).toBe(3);
     });
 
     it("returns a 'disposer' function that can remove the callback from the store", () => {
-      expect(observerCalls).toBe(1);
-      todo.title = "yeah";
+      //   expect(observerCalls).toBe(1);
+      todo.setTitle("yeah");
       expect(observerCalls).toBe(2);
       disposer();
-      todo.title = "great";
+      todo.setTitle("great");
       expect(observerCalls).toBe(2);
     });
   });
@@ -72,9 +92,17 @@ describe("MobX Todo", () => {
           this.title = "";
           this.priority = 0;
         }
+
+        setTitle(v) {
+          this.title = v;
+        }
       };
 
-      decorate(Todo, { title: observable, priority: observable });
+      decorate(Todo, {
+        title: observable,
+        priority: observable,
+        setTitle: action
+      });
 
       todo = new Todo();
 
@@ -88,41 +116,58 @@ describe("MobX Todo", () => {
     it("lets a function as a reaction to store changes", () => {
       // autorun() runs the callback function immediately to learn which properties it looks at.
       expect(observerCalls).toBe(1);
-      todo.title = "yeah";
+      todo.setTitle("yeah");
       expect(observerCalls).toBe(2);
-      todo.title = "great";
+      todo.setTitle("great");
       expect(observerCalls).toBe(3);
     });
 
     it("returns a 'disposer' function that can remove the callback from the store", () => {
       expect(observerCalls).toBe(1);
-      todo.title = "yeah";
+      todo.setTitle("yeah");
       expect(observerCalls).toBe(2);
       disposer();
-      todo.title = "great";
+      todo.setTitle("great");
       expect(observerCalls).toBe(2);
     });
   });
 
   describe("computed property", () => {
     let reactionCallbackCalls = 0;
-    const square = observable({
-      width: 0,
-      get area() {
-        return this.width * this.width;
+    class Square {
+      constructor() {
+        extendObservable(
+          this,
+          {
+            width: 0,
+
+            get area() {
+              return this.width * this.width;
+            },
+
+            setWidth(v) {
+              this.width = v;
+            }
+          },
+          {
+            setWidth: action
+          }
+        );
       }
-    });
+    }
+
+    const square = new Square();
 
     autorun(() => {
       reactionCallbackCalls++;
       square.area;
     });
 
-    it("triggers reactions", () => {
+    it("triggers reactions when a referenced property changes", () => {
       expect(reactionCallbackCalls).toBe(1);
-      square.width = 2;
+      square.setWidth(2);
       expect(reactionCallbackCalls).toBe(2);
-      square.width = 2;
+      square.setWidth(2);
       expect(reactionCallbackCalls).toBe(2);
     });
   });
